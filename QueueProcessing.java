7a7fa6cadeb722e36722f52156411afaa08eefb2
Stack681 <50956899+Stack681@users.noreply.github.com>
2020-08-09 21:20:00 -0400

    Add files via upload
    Added some stuff.

--- QueueProcessing.java
+++ QueueProcessing.java
@@ -1,28 +1,37 @@
 import ch04.queues.ArrayBoundedQueue;
 
 public class QueueProcessing {
+
     public static void main(String[] args) {
 
+        // Create new Ticket Queue
         TixQueue newTixQue = new TixQueue(10,10000);
-        //Start the second thread
+
+        // Move the new Ticket Queue object into the second thread
         Thread t = new Thread(newTixQue);
         t.start();
 
-//        TixOrder testOrder = new TixOrder(, tixCount);
+//        TixOrder testOrder = new TixOrder("Someone", 3);
 //        newTixQue.enqueue(testOrder);
 //        testOrder.order();
 
-//        // Create and enqueue order 1
-//        TixOrder orderOne = new TixOrder("Elise", 5);
-//        newTixQue.enqueue(orderOne);
-//        System.out.println("Current available tickets: " + newTixQue.getTotTix());
-//
-//        // Create and enqueue order 2
-//        TixOrder orderTwo = new TixOrder("Andrew", 15);
-//        newTixQue.enqueue(orderTwo);
+        System.out.println("Current available tickets: " + newTixQue.getTotTix());
 
-        // Process orders
-        System.out.println(newTixQue.dequeueOrder());
-        System.out.println(newTixQue.dequeueOrder());
+        boolean contRun = true;
+
+        do {
+            // Create amd enqueue a ticket order
+            TixOrder orderOne = new TixOrder();
+            newTixQue.enqueue(orderOne);
+        } while (contRun);
+
+
+//        // Create and enqueue order 2
+//        TixOrder orderTwo = new TixOrder();
+//        newTixQue.enqueue(orderTwo);
+//        System.out.println(newTixQue.dequeueOrder());
+//
+//        // Process orders
+
     }
 }
